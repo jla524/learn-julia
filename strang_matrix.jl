fe1be1830aa16b3540db31197ac4d89d5640c889
Jacky Lee <jla524@sfu.ca>
2021-04-24 12:19:17 -0700

    Add strang_matrix.jl

---
+++ strang_matrix.jl
@@ -0,0 +1,17 @@
+# Use Julia's array and control flow syntax in order to define the NxN 
+# Strang matrix.
+
+
+N = 10
+A = zeros(Int8, N, N)
+
+for i=1:N
+  A[i, i] = -2
+end
+
+for i=2:N
+  A[i, i-1] = 1
+  A[i-1, i] = 1
+end
+
+show(stdout, "text/plain", A)
