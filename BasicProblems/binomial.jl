a7f647f1b6bbbcfdee82a509b977df199b18a2af
Jacky Lee <jla524@sfu.ca>
2021-04-25 15:54:14 -0700

    Add binomial.jl

---
+++ BasicProblems/binomial.jl
@@ -0,0 +1,13 @@
+# Using only rand(), write a function binomial_rv(n,p) that produces one draw
+# of Bin(n, p).
+
+
+function binomial_rv(n, p)
+    rv = 0
+    for i=1:n
+        rand() <= p && (rv += 1)
+    end
+    return rv
+end
+
+print([binomial_rv(10, 0.5) for i in 1:10])
