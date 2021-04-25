d7c9f53aba9fdeaeff544b21b5e5b4524d586cca
Jacky Lee <jla524@sfu.ca>
2021-04-25 16:26:27 -0700

    Add monte_carlo.jl

---
+++ BasicProblems/monte_carlo.jl
@@ -0,0 +1,12 @@
+function monte_carlo(n)
+    land = 0
+    for i=1:n
+        x, y = rand(Float64, 2)
+        x = x * 2 - 1
+        y = y * 2 - 1
+        land += (x ^ 2 + y ^ 2 <= 1)
+    end
+    return land * 4.0 / n
+end
+
+print(monte_carlo(1e7))
