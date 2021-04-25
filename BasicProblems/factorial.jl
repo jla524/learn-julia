da4c7580710a64966e1802b03e502b891840d433
Jacky Lee <jla524@sfu.ca>
2021-04-24 22:12:33 -0700

    Add BasicProblems folder and fix indents

--- BasicProblems/factorial.jl
+++ BasicProblems/factorial.jl
@@ -3,11 +3,11 @@
 
 
 function my_factorial(n)
-ans = 1
-  for i = 1:n
-    ans *= i
-  end
-  return ans
+    ans = 1
+    for i = 1:n
+        ans *= i
+    end
+    return ans
 end;
 
 print(my_factorial(big(100)))
--- BasicProblems/strang_matrix.jl
+++ BasicProblems/strang_matrix.jl
@@ -6,9 +6,9 @@ N = 10
 A = zeros(Int8, N, N)
 
 for i=1:N
-  A[i, i] = -2
-  (i > 1) && (A[i, i-1] = 1)
-  (i > 1) && (A[i-1, i] = 1)
+    A[i, i] = -2
+    (i > 1) && (A[i, i-1] = 1)
+    (i > 1) && (A[i-1, i] = 1)
 end
 
 show(stdout, "text/plain", A)
