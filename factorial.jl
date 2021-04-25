c89b346cc2d9414763810683ca25497a07e06e92
Jacky Lee <jla524@sfu.ca>
2021-04-24 22:01:54 -0700

    Add factorial.jl

---
+++ factorial.jl
@@ -0,0 +1,13 @@
+# Using a for loop, write a function my_factorial(n) that computes the nth 
+# factorial.
+
+
+function my_factorial(n)
+ans = 1
+  for i = 1:n
+    ans *= i
+  end
+  return ans
+end;
+
+print(my_factorial(big(100)))
