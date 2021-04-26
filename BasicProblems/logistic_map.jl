61fcc9eabac3aedbce1ccd2739afabf2744065da
Jacky Lee <jla524@sfu.ca>
2021-04-25 22:30:07 -0700

    Add logistic_map.jl

---
+++ BasicProblems/logistic_map.jl
@@ -0,0 +1,30 @@
+# The logistic difference equation is defined by the recursion 
+# "b_{n+1}=r*b_{n}(1-b_{n})" where "b_n" is the number of bunnies at time n.
+# Starting with "b_0 = .25", by around 400 iterations this will reach a steady
+# state. This steady state is dependent on r. Write a function which plots the
+# steady state attractor.
+
+
+using Plots
+
+r = 2.9:.001:4
+b = ones(length(r)) * .25
+X = ones(length(r), 150)
+
+for i in 1:400
+    @. b = r * b * (1 - b)
+end
+
+X[:, 1] = b
+
+for i in 1:149
+    @. X[:, i+1] = r * X[:, i] * (1 - X[:, i])
+end
+
+plot(collect(r), X,
+     seriestype=:scatter,
+     markersize=.002, 
+     legend=false,
+     color=:black
+)
+savefig("output")
