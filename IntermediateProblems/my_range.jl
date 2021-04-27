db485d69595b5167746be7ecff099dc73a4961c5
Jacky Lee <jla524@sfu.ca>
2021-04-26 21:14:33 -0700

    Add my_range.jl

---
+++ IntermediateProblems/my_range.jl
@@ -0,0 +1,24 @@
+# Create a type MyRange which stores start, step, and stop. After getting
+# this correct, use the Julia array interface in order to define the function
+# for the a[i] syntax on your type.
+
+
+# Okay, here we go again ._.
+struct MyRange
+    start
+    step
+    stop
+end
+
+
+function Base.getindex(a::MyRange, i)
+    # Use i - 1 since index starts at 1
+    rv = a.start + (i - 1) * a.step
+    rv > a.stop && (error("Index is out of bounds!"))
+    return rv
+end
+
+
+arr = MyRange(1, 2, 20)
+result = arr[5]
+println(result)
