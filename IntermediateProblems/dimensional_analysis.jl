4c21b82ad1e039594ab8bd15244fdb703c922904
Jacky Lee <jla524@sfu.ca>
2021-05-01 09:57:55 -0700

    Add dimensional_analysis.jl

---
+++ IntermediateProblems/dimensional_analysis.jl
@@ -0,0 +1,31 @@
+# Do your implementations *obey* dimensional analysis? Try using the package
+# Unitful to build arrays of numbers with units, and see if you can make 
+# your LinSpace not give errors.
+
+
+# Struct and functions are copied from linspace.jl
+struct LinSpace
+    start
+    stop
+    N
+end
+
+
+function Base.size(a::LinSpace)
+    return (a.N,)
+end
+
+
+function Base.getindex(a::LinSpace, i)
+    (a.N == 1 && a.start == a.stop) && return a.start 
+    (a.N <= 1 && a.start != a.stop) && (error("endpoints differ"))
+    (i <= 0 || i > a.N) && (error("index is out of bound"))
+    step = (a.stop - a.start) / (a.N - 1) 
+    return a.start + (i - 1) * step
+end
+
+
+using Unitful
+obj = LinSpace(1u"kg", 10u"kg", 10)
+println(obj)
+println(obj[5])
