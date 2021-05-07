0a180ac16f83ba4fec0c8104b5932f3638341664
Jacky Lee <jla524@sfu.ca>
2021-05-06 23:47:11 -0700

    Add metaprogramming.jl

---
+++ AdvancedProblems/metaprogramming.jl
@@ -0,0 +1,14 @@
+# Implement your own version of the @evalpoly macro called @myevalpoly
+
+
+macro myevalpoly(x, a...)
+    rv = a[end]
+    for i in length(a)-1:-1:1
+        rv = rv * x + a[i]
+    end
+    return rv
+end
+
+
+println(@myevalpoly 7 2 3 4 5)
+println(@evalpoly 7 2 3 4 5)
