016adf1bace582e0871d9d96491edccf0dd666c9
Jacky Lee <jla524@sfu.ca>
2021-12-25 20:23:52 -0800

    Add line breaks

--- Clustering/advanced.jl
+++ Clustering/advanced.jl
@@ -1,11 +1,6 @@
-# For the the example presented here, we will use a subhset of Word Embedding, trained using Word2Vec.jl. These are 100 dimentional vectors, which encode syntactic and semantic information about words.
-# Use Affinity Propagraion from Clustering.jl, to cluster word2vec word embeddings, according to meaning.
-# Done right this will seperate locations from sports
-# Done finely and it will seperate ball-sports from other sports, and will seperate locations according to regions, etc
-# Affinity propagraion requires a similarity matrix, which you can set as a negated distance matrix.
-# For this you'll also want Distances.jl for all your distance metric needs.
-# It is traditional with word2vec to use cosine distance.
-# You will as also need to set each item's availability. This is the diagonal of the similarity matrix. Decreasing it roughly corresponds to decreasing the amount each node wants to be in a cluster on its own.
+# For the the example presented here, we will use a subhset of Word Embedding,
+# trained using Word2Vec.jl. These are 100 dimentional vectors, which encode
+# syntactic and semantic information about words.
 
 using JSON
 using Embeddings
@@ -30,7 +25,7 @@ similarity[diagind(similarity)] .= 0.01
 aprop = affinityprop(similarity)
 
 for (i, examplar) in enumerate(aprop.exemplars)
-    println("-"^32)
+    println("-" ^ 32)
     println("Exemplar: ", all_words[examplar])
     member_indices = findall(assignments(aprop) .== i)
     println(join(getindex.([all_words], member_indices), ", "))
