815e9d6b268a231ee20d8845dce261b011a5c070
Jacky Lee <jla524@sfu.ca>
2021-12-30 02:26:21 -0800

    Add visualization

---
+++ DataVisualization/regression_plot.jl
@@ -0,0 +1,19 @@
+# Make a beautiful plot of your regression
+
+using Loess, Plots
+
+
+xs = 12 .* rand(100)
+ys = cos.(xs) .+ 0.5 * rand(100)
+
+model = loess(xs, ys, span=0.5)
+us = range(extrema(xs)...; step=0.1)
+vs = predict(model, us)
+
+scatter(xs, ys, title="Regression Plot", label="data")
+plot!(x -> cos(x) + 0.25, 0, 12, label="linear regression")
+plot!(us, vs, label="smoothed")
+
+xlabel!("x")
+ylabel!("y")
+savefig("output")
